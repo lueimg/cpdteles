7de00613728de1bbb6c49e70a4eca893a0063ce2
Luis Mori <luismori@glr.pe>
2014-04-16 10:45:25 -0500

    word tablas

--- reporte/word/WORDpreMatriculaAdm.php
+++ reporte/word/WORDpreMatriculaAdm.php
@@ -1,27 +1,28 @@
 <?php
-/*conexion*/
+
+/* conexion */
 require_once '../../conexion/MySqlConexion.php';
 require_once '../../conexion/configMySql.php';
-/*crea obj conexion*/
+/* crea obj conexion */
 require_once 'PHPWord.php';
 
-$cn=MySqlConexion::getInstance();
+$cn = MySqlConexion::getInstance();
 
 
-/* QUERYS*/
-$cgracpr=$_GET['cgracpr'];
-$cingalu=$_GET['cingalu'];
-$semestre=$_GET['csemaca'];
-$cfilial=$_GET['cfilial'];
-$cinstit=$_GET['cinstit'];
-$alumno="";
+/* QUERYS */
+$cgracpr = $_GET['cgracpr'];
+$cingalu = $_GET['cingalu'];
+$semestre = $_GET['csemaca'];
+$cfilial = $_GET['cfilial'];
+$cinstit = $_GET['cinstit'];
+$alumno = "";
 
-if(trim($cingalu)!=""){
-$listAlum=str_replace(",","','",$cingalu);
-$alumno=" AND co.cingalu in ('".$listAlum."')";
+if (trim($cingalu) != "") {
+  $listAlum = str_replace(",", "','", $cingalu);
+  $alumno = " AND co.cingalu in ('" . $listAlum . "')";
 }
 
-$sql="	Select i.cingalu,ins.durllog,fi.dfilial,i.dcoduni,replace(i.dcodlib,'-','') as dcodlib,co.cconmat,gr.csemaca,ca.dcarrer,ca.durlcam,pe.dnomper,pe.dappape,pe.dapmape,ci.dciclo,
+$sql = "	Select i.cingalu,ins.durllog,fi.dfilial,i.dcoduni,replace(i.dcodlib,'-','') as dcodlib,co.cconmat,gr.csemaca,ca.dcarrer,ca.durlcam,pe.dnomper,pe.dappape,pe.dapmape,ci.dciclo,
 		GROUP_CONCAT(concat(
 			cu.codicur,'|',cu.dcurso,'|',dg.ncredit) SEPARATOR '^^') as cursos,
 		(select CONCAT(co.nprecio,'|',rr.fvencim)
@@ -66,110 +67,116 @@ $sql="	Select i.cingalu,ins.durllog,fi.dfilial,i.dcoduni,replace(i.dcodlib,'-','
 		LEFT JOIN cursom cu ON (cu.ccurso=dg.ccurso)
 		INNER JOIN cicloa ci ON (ci.cciclo=gr.cciclo)
 		INNER JOIN carrerm ca ON (ca.ccarrer=gr.ccarrer)
-		where co.cgruaca in ('".str_replace(",","','",$cgracpr)."')	
-		".$alumno."
+		where co.cgruaca in ('" . str_replace(",", "','", $cgracpr) . "')	
+		" . $alumno . "
 		GROUP BY co.cingalu,co.cgruaca";
 $cn->setQuery($sql);
-$alumno=$cn->loadObjectList();
+$alumno = $cn->loadObjectList();
 
 // print json_encode($alumno);
-
-
 // Create a new PHPWord Object
 $PHPWord = new PHPWord();
 
 // Every element you want to append to the word document is placed in a section. So you need a section:
 $section = $PHPWord->createSection();
 
-// After creating a section, you can append elements:
-// $section->addText('Hello world!');
 
-// You can directly style your text by giving the addText function an array:
-// $section->addText('Hello world! I am formatted.', array('name'=>'Tahoma', 'size'=>16, 'bold'=>true,'align'=>'center',));
-
-// If you often need the same style again you can create a user defined style to the word document
-// and give the addText function the name of the style:
-// $PHPWord->addFontStyle('myOwnStyle', array('name'=>'Verdana', 'size'=>14, 'color'=>'1B2232'));
-// $section->addText('Hello world! I am formatted by a user defined style', 'myOwnStyle');
-
-// // You can also putthe appended element to local object an call functions like this:
-// $myTextElement = $section->addText('Hello World!');
-// $myTextElement->setBold();
-// $myTextElement->setName('Verdana');
-// $myTextElement->setSize(22);
+foreach ($alumno as $rs) { // Inicio de Recorrido
+  $detfechamatric = explode("|", $rs['matricula_fecha']);
+  $rs['matricula'] = $detfechamatric[0];
+  $rs['fecha_matric'] = $detfechamatric[1];
+  $detfechainscrip = explode("|", $rs['inscripcion']);
+  $monto_inscripcion = $detfechainscrip[0];
+  $fecha_inscripcion = $detfechainscrip[1];
 
 
 
+  $section->addText('CONSTANCIA DE PRE-MATRICULA', array('bold' => true, 'size' => 11, 'name' => 'Calibri',), array('align' => 'center'));
+  $section->addText('', array('bold' => true, 'size' => 10, 'name' => 'Calibri',), array('align' => 'center'));
+  $section->addText('CODIGO					:' . $rs['dcodlib'], array('bold' => true, 'size' => 10, 'name' => 'Calibri',));
+  $section->addText('APELLIDOS Y NOMBRE				:' . $rs['dnomper'] . " " . $rs['dappape'] . " " . $rs['dapmape'], array('bold' => true, 'size' => 10, 'name' => 'Calibri',));
+  $section->addText('LOCAL DE ESTUDIOS				:' . $rs['dfilial'], array('bold' => true, 'size' => 10, 'name' => 'Calibri',));
+  $section->addText('CARRERA PROFESIONAL			:' . $rs['dcarrer'], array('bold' => true, 'size' => 10, 'name' => 'Calibri',));
+  $section->addText('FECHA DE MATRICULA				:' . $rs['fecha_matric'], array('bold' => true, 'size' => 10, 'name' => 'Calibri',));
+  $section->addText('FECHA DE INICIO DE CLASES			:' . $rs['finicio'], array('bold' => true, 'size' => 10, 'name' => 'Calibri',));
+  $section->addText('', array('bold' => true, 'size' => 10, 'name' => 'Calibri',), array('align' => 'center'));
 
-foreach($alumno as $rs){ // Inicio de Recorrido
-	$section->addText(json_encode($rs));
-	$detfechamatric=explode("|",$rs['matricula_fecha']);
-	$rs['matricula']=$detfechamatric[0];
-	$rs['fecha_matric']=$detfechamatric[1];
-	$detfechainscrip=explode("|",$rs['inscripcion']);
-	$monto_inscripcion=$detfechainscrip[0];
-	$fecha_inscripcion=$detfechainscrip[1];
-
-// ->setCellValue('F3',$rs['dcodlib'])
-// 			->setCellValue('F4',$rs['dnomper']." ".$rs['dappape']." ".$rs['dapmape'])
-// 			->setCellValue('F5',$rs['dfilial'])
-// 			->setCellValue('F6',$rs['dcarrer'])
+  $dcursos = explode("^^", $rs['cursos']);
+  $detcic = explode(" ", $rs['dciclo']);
 
 
-$section->addText('CONSTANCIA DE PRE-MATRICULA', array('bold'=>true, 'size'=>11, 'name'=>'Calibri', ),array('align'=>'center'));
-$section->addText('', array('bold'=>true, 'size'=>11, 'name'=>'Calibri', ),array('align'=>'center'));
-$section->addText('CODIGO					:'.$rs['dcodlib'], array('bold'=>true, 'size'=>11, 'name'=>'Calibri', ));
-$section->addText('APELLIDOS Y NOMBRE				:'.$rs['dnomper']." ".$rs['dappape']." ".$rs['dapmape'], array('bold'=>true, 'size'=>11, 'name'=>'Calibri', ));
-$section->addText('LOCAL DE ESTUDIOS				:'.$rs['dfilial'], array('bold'=>true, 'size'=>11, 'name'=>'Calibri', ));
-$section->addText('CARRERA PROFESIONAL			:'.$rs['dcarrer'], array('bold'=>true, 'size'=>11, 'name'=>'Calibri', ));
-$section->addText('FECHA DE MATRICULA				:', array('bold'=>true, 'size'=>11, 'name'=>'Calibri', ));
-$section->addText('FECHA DE INICIO DE CLASES			:', array('bold'=>true, 'size'=>11, 'name'=>'Calibri', ));
+  if (trim($rs['cursos']) == "") {
+    
+  } else {
+    $debug = 1;
+    $table = $section->addTable();
+    $table->addRow();
+    $table->addCell(2000)->addText("PROGRAMACION ACADEMICA ".$rs["csemaca"]);
+    $table->addRow();
+      // Add cells
+      $table->addCell(500)->addText("CICLO");
+      $table->addCell(500)->addText("CODIG");
+      $table->addCell(2000)->addText("ASIG");
+      $table->addCell(500)->addText("CRED");
+      //SUMA TOTAL DE CREDITOS AL FINAL DE LA TABLA
+      $credito = 0;
+    for ($i = 11; $i < (11 + count($dcursos)); $i++) { //Antiguo era hasta el 16
+      $ddc = explode("|", $dcursos[($i - 11)]);
+      
+      $table->addRow();
+      // Add cells
+      $table->addCell(500)->addText($detcic[0]);
+      $table->addCell(500)->addText($ddc[0]);
+      $table->addCell(2000)->addText($ddc[1]);
+      $table->addCell(500)->addText($ddc[2]);
+      $credito += $ddc[2];
+    }
+    $table->addRow();
+      // Add cells
+      $table->addCell(3000)->addText("TOTAL CREDITOS");
+      $table->addCell(500)->addText($credito); 
+  } // FIN TABLA CURSOS
+  $debug = 1;
+  //PAGOS PARA LA MATRICULA
+  $table = $section->addTable();
+    $table->addRow();
+    $table->addCell(2000)->addText("PAGOS PARA LA MATRICULA ");
+    $table->addRow();
+      // Add cells
+      $table->addCell(500)->addText("CONCEPTOS");
+      $table->addCell(500)->addText("FECHAS");
+      $table->addCell(2000)->addText("IMPORTE");
+  $table->addRow();
+      // Add cells
+      $table->addCell(500)->addText("Inscripción");
+      $table->addCell(500)->addText("FECHAS");
+      $table->addCell(2000)->addText("IMPORTE");
+  $table->addRow();
+      // Add cells
+      $table->addCell(500)->addText("Derecho de Matrícula");
+      $table->addCell(500)->addText("FECHAS");
+      $table->addCell(2000)->addText("IMPORTE");
+  $table->addRow();
+      // Add cells
+      $table->addCell(500)->addText("Pensión de la Carrera");
+      $table->addCell(500)->addText("FECHAS");
+      $table->addCell(2000)->addText("IMPORTE");    
+      
+      
+  
+}//FIN ALUMNOS
 
 
-$dcursos=explode("^^",$rs['cursos']);
-$detcic=explode(" ",$rs['dciclo']);
-	$section->addText(json_encode($dcursos));
-	$section->addText(json_encode($detcic));
-
-if(trim($rs['cursos'])==""){
-// $objPHPExcel->getActiveSheet()->setCellValue('A11','PROCESO DE CONVALIDACIÓN');
-// $objPHPExcel->getActiveSheet()->mergeCells('A11:E11');
-// $objPHPExcel->getActiveSheet()->getStyle('A11:E11')->applyFromArray($styleAlignmentBold);
-// $posfin=12;
-}
-else{
-for($i=11;$i<(11+count($dcursos));$i++){ //Antiguo era hasta el 16
-	$ddc=explode("|",$dcursos[($i-11)]);
-	// $objPHPExcel->getActiveSheet()->setCellValue('A'.$i,$detcic[0]);
-	// $objPHPExcel->getActiveSheet()->setCellValue('C'.$i,$ddc[1]);
-	// $objPHPExcel->getActiveSheet()->getStyle('C'.$i)->getAlignment()->setWrapText(true);
-	// $objPHPExcel->getActiveSheet()->getRowDimension($i)->setRowHeight(21.5); // altura
-	// $objPHPExcel->getActiveSheet()->mergeCells('C'.$i.':D'.$i);	
-	// $objPHPExcel->getActiveSheet()->setCellValue('E'.$i,$ddc[2]);
-	// $objPHPExcel->getActiveSheet()->setCellValue('B'.$i,$ddc[0]); // se agrego recientemente
-	}
-$posfin=$i;
-}
 
 
 
-
-
-}
-
-
-
-
-
-
-// At least write the document to webspace:
-$objWriter = PHPWord_IOFactory::createWriter($PHPWord, 'Word2007');
 $nombre = 'PreMatricula.docx';
-$objWriter->save($nombre);
+// Redirect output to a client's web browser (Excel2007)
+header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-Disposition: attachment;filename="' . $nombre . '"');
+header('Cache-Control: max-age=0');
 
-header ("Location:$nombre");
-// print "hola";
-
-
-
-
+$objWriter = PHPWord_IOFactory::createWriter($PHPWord, 'Word2007');
+$objWriter->save('php://output');
+exit;
+?>
