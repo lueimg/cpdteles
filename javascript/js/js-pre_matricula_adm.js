ea5c78ddc02e8cc67419f07d009941346a4f2fcf
luis mori <luismorith@gmail.com>
2014-04-18 00:51:25 -0500

    exportacion en pdf

--- javascript/js/js-pre_matricula_adm.js
+++ javascript/js/js-pre_matricula_adm.js
@@ -62,6 +62,11 @@ VisualizarGruposHTML=function(obj){
         '        	<i class="icon-white icon-folder-open"></i>'+
         '       </a>'+
         ' 	</div>'+
+        '	<div style="margin:15px 0px 10px 0px;">'+
+		'		<a onClick="ExportarGrupoPDF('+"'"+obj[i].id+"','','"+obj[i].total+"'"+')" class="btn btn-azul sombra-3d t-blanco" href="javascript:void(0)">'+
+        '        	<i class="icon-white icon-folder-open">W</i>'+
+        '       </a>'+
+        ' 	</div>'+
 		'</td>';
 	htm+="</tr>";
 	}
@@ -105,7 +110,7 @@ VisualizarDetalleHTML=function(obj){
         ' 	</div>'+
 
         '	<div style="margin:15px 0px 10px 0px;">'+
-		'		<a onClick="ExportarGrupoWord('+"'"+obj[i].id+"','"+obj[i].cingalu+"','1'"+')" class="btn btn-azul sombra-3d t-blanco" href="javascript:void(0)">'+
+		'		<a onClick="ExportarGrupoPDF('+"'"+obj[i].id+"','"+obj[i].cingalu+"','1'"+')" class="btn btn-azul sombra-3d t-blanco" href="javascript:void(0)">'+
         '        	<i class="icon-white icon-folder-open">W</i>'+
         '       </a>'+
         ' 	</div>'+
@@ -127,6 +132,15 @@ VisualizarDetalleHTML=function(obj){
 		'	<div style="margin:15px 0px 10px 0px;">'+
 		'		<a onClick="ExportarGrupo2('+"'"+idgrupo+"'"+')" class="btn btn-azul sombra-3d t-blanco" href="javascript:void(0)">'+
         '        	Exportar Seleccionados<i class="icon-white icon-folder-open"></i>'+
+        '       </a>'+
+        ' 	</div>'+
+		'</td>';
+	htm2+="</tr>";
+	htm2+="<tr>";
+	htm2+="<td width='800' class='t-center'>"+
+		'	<div style="margin:15px 0px 10px 0px;">'+
+		'		<a onClick="ExportarGrupo2PDF('+"'"+idgrupo+"'"+')" class="btn btn-azul sombra-3d t-blanco" href="javascript:void(0)">'+
+        '        	Exportar Seleccionados PDF<i class="icon-white icon-folder-open"></i>'+
         '       </a>'+
         ' 	</div>'+
 		'</td>';
@@ -165,6 +179,38 @@ ExportarGrupo2=function(grupo){
 	}
 }
 
+ExportarGrupo2PDF=function(grupo){
+	var act=false;
+	var contador=0;
+	var alumnos=$(".check").map(function(index, element) {
+        if($(this).attr("checked")){
+			contador++;
+			act=true;
+		return this.value;		
+		}
+    }).get().join(",");
+	//alert(alumnos);
+	
+	if(act==true && contador<=30){
+	// window.location='../reporte/pdf/PDFpreMatriculaAdm.php?cgracpr='
+ //                	+grupo+'&cingalu='+alumnos+
+	// 				'&csemaca='+$("#slct_semestre").val()+
+	// 				'&cfilial='+$("#slct_filial").val()+
+	// 				'&cinstit='+$("#slct_instituto").val();
+	window.open('../reporte/pdf/PDFpreMatriculaAdm.php?cgracpr='
+                	+grupo+'&cingalu='+alumnos+
+					'&csemaca='+$("#slct_semestre").val()+
+					'&cfilial='+$("#slct_filial").val()+
+					'&cinstit='+$("#slct_instituto").val(), "_blank");
+	}
+	// else if(act==true && contador>30){
+	// sistema.msjAdvertencia("El max para enviar es de 30 ud selecciono "+contador,3000);
+	// }
+	else{
+	sistema.msjAdvertencia("Seleccione almenos un registro",3000);
+	}
+}
+
 ExportarGrupo=function(grupo,alumno,t){
 	if(t<=30){
 	window.location='../reporte/excel/EXCELpreMatriculaAdm.php?cgracpr='
@@ -178,15 +224,21 @@ ExportarGrupo=function(grupo,alumno,t){
 	}
 }
 
-ExportarGrupoWord=function(grupo,alumno,t){
-	if(t<=30){
-	window.location='../reporte/word/WORDpreMatriculaAdm.php?cgracpr='
+ExportarGrupoPDF=function(grupo,alumno,t){
+	// if(t<=30){
+	// window.location='../reporte/pdf/PDFpreMatriculaAdm.php?cgracpr='
+ //                	+grupo+'&cingalu='+alumno+
+	// 				'&csemaca='+$("#slct_semestre").val()+
+	// 				'&cfilial='+$("#slct_filial").val()+
+	// 				'&cinstit='+$("#slct_instituto").val();
+
+	window.open('../reporte/pdf/PDFpreMatriculaAdm.php?cgracpr='
                 	+grupo+'&cingalu='+alumno+
 					'&csemaca='+$("#slct_semestre").val()+
 					'&cfilial='+$("#slct_filial").val()+
-					'&cinstit='+$("#slct_instituto").val();
-	}
-	else{
-	sistema.msjAdvertencia("El max para enviar es de 30 ud tiene "+t,3000);
-	}
+					'&cinstit='+$("#slct_instituto").val() , "_blank");				
+	// }
+	// else{
+	// sistema.msjAdvertencia("El max para enviar es de 30 ud tiene "+t,3000);
+	// }
 }
