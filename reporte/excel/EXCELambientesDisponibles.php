d49f45b650a187d9d46ae23b43eafff10502157e
luis mori <luismorith@gmail.com>
2014-06-18 01:58:57 -0500

    reporte de ambirentes disponibles , falta probarlo

--- reporte/excel/EXCELambientesDisponibles.php
+++ reporte/excel/EXCELambientesDisponibles.php
@@ -48,21 +48,26 @@ if(!empty($institucion))
 
 $sql  ="
 select  
- hora.chora
-,CONCAT(hora.hinici,' - ',hora.hfin) dhora
-,dia.cdia
-,dia.dnomdia 
-,ho.cambien
-,amb.numamb
-,ins.dinstit
-,ins.cinstit
-from horprop ho
-inner join cuprprp cupr on cupr.ccuprpr = ho.ccurpro
-inner join gracprp gra on gra.cgracpr = cupr.cgracpr
-inner join diasm dia on dia.cdia = ho.cdia
-inner join horam hora on hora.chora = ho.chora
-inner join instita ins on ins.cinstit = hora.cinstit
-inner join ambienm amb on amb.cambien = ho.cambien
+hora.chora ,
+CONCAT(hora.hinici,' - ',hora.hfin) dhora ,dia.cdia ,dia.dnomdia ,ho.cambien ,amb.numamb 
+,ins.dinstit 
+,ins.cinstit 
+,gra.csemaca
+,gra.cinicio
+,cur.dcurso
+,car.dcarrer
+,CONCAT_WS(' ',per.dappape,per.dapmape,per.dnomper) nombre
+from horprop ho 
+inner join cuprprp cupr on cupr.ccuprpr = ho.ccurpro 
+inner join gracprp gra on gra.cgracpr = cupr.cgracpr 
+inner join diasm   dia on dia.cdia = ho.cdia 
+inner join horam   hora on hora.chora = ho.chora 
+inner join instita ins on ins.cinstit = hora.cinstit 
+inner join ambienm amb on amb.cambien = ho.cambien 
+inner join cursom  cur on cur.ccurso = cupr.ccurso
+inner join carrerm car on car.ccarrer = gra.ccarrer
+inner join profesm pro on pro.cprofes = ho.cprofes
+inner join personm per on per.cperson = pro.cperson
 where  1=1
 and amb.cfilial = $filial
 $where
@@ -73,13 +78,17 @@ $cn->setQuery($sql);
 $horarios=$cn->loadObjectList();
 
 
-//print "<pre>". var_dump(expression)."</pre>";
 
 foreach ($horarios as $row ) {
 	# code...
 
 	$grilla[$row['chora']]['texto']= $row['dhora'];
-	$grilla[$row['chora']]['dias'][$row['dnomdia']][] = $row["numamb"];
+	$grilla[$row['chora']]['dias'][$row['dnomdia']][] = $row["dinstit"] 
+								. '<br>SEMESTRE: ' . $row["csemaca"]. '/'.$row["cinicio"]
+								. '<br>'.$row["dcarrer"]
+								. '<br>'.$row["dcurso"]
+								. '<br>'.$row["nombre"]
+								;
 
 
 }
@@ -87,25 +96,38 @@ foreach ($horarios as $row ) {
 
 
 foreach ($grilla as  $row) {
-	$tr_horario .="<tr><td>".$row['texto']."</td><td>"
-	. @implode('<br>', $row['dias']['LUNES']) ."</td><td>"
-	. @implode('<br>', $row['dias']['MARTES']) ."</td><td>"
-	. @implode('<br>', $row['dias']['MIERCOLES']) ."</td><td>"
-	. @implode('<br>', $row['dias']['JUEVES']) ."</td><td>"
-	. @implode('<br>', $row['dias']['VIERNES']) ."</td><td>"
-	. @implode('<br>', $row['dias']['SABADO']) ."</td><td>"
-	. @implode('<br>', $row['dias']['DOMINGO']) ."</td></tr>";
+	$tr_horario .="<tr><td style=\"text-align:center;vertical-align:middle;\">".$row['texto']."</td><td style=\"text-align:center;vertical-align:middle;\">"
+	. @implode('<hr>', $row['dias']['LUNES']) ."</td><td style=\"text-align:center;vertical-align:middle;\">"
+	. @implode('<hr>', $row['dias']['MARTES']) ."</td><td style=\"text-align:center;vertical-align:middle;\">"
+	. @implode('<hr>', $row['dias']['MIERCOLES']) ."</td><td style=\"text-align:center;vertical-align:middle;\">"
+	. @implode('<hr>', $row['dias']['JUEVES']) ."</td><td style=\"text-align:center;vertical-align:middle;\">"
+	. @implode('<hr>', $row['dias']['VIERNES']) ."</td><td style=\"text-align:center;vertical-align:middle;\">"
+	. @implode('<hr>', $row['dias']['SABADO']) ."</td><td style=\"text-align:center;vertical-align:middle;\">"
+	. @implode('<hr>', $row['dias']['DOMINGO']) ."</td></tr>";
 	
 }
+$mes = array("","enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre");
 
 
+$fecha = "Lima, ".date("d").' de '.$mes[date('m')]. ' del '.date("Y");
 $html = <<<EOD
+
 <table>
-<tr><td colspan="2">Filial : {$dfilial}</td></tr>
-{$tableInstitucion}
+<tr><td colspan="8" style="font-size:2em;text-align:center">GRUPO EDUCATIVO TELESUP</td></tr>
+<tr><td colspan="8" style="font-size:1.5em;text-align:center" >DISPONIBILIDAD DE AMBIENTES</td></tr>
+<tr>
+<td colspan="2" style="text-align:left">ODE: {$dfilial}</td>
+<td colspan="2" style="text-align:left"></td>
+<td colspan="2" style="text-align:left"></td>
+<td colspan="2" style="text-align:right">{$fecha}</td>
+</tr>
 </table>
-<table border=1 style="vertical-align:middle">
-<tr><td>Hora</td><td>LUNES</td><td>MARTES</td><td>MIERCOLES</td><td>JUEVES</td><td>VIERNES</td><td>SABADO</td><td>DOMINGO</td></tr>
+
+<p></p>
+<table border=1>
+<tr style="text-align:center; background:#A6D2F7;font-weight:bold">
+<td style="width:300px">Hora</td><td style="width:300px">LUNES</td><td style="width:300px">MARTES</td><td style="width:300px">MIERCOLES</td><td style="width:300px">JUEVES</td><td style="width:300px">VIERNES</td><td style="width:300px">SABADO</td><td style="width:300px">DOMINGO</td>
+</tr>
 {$tr_horario}
 </table>
 
